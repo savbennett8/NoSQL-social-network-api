a2bc4a1cbcdb165ca43a50310acee2c7d5e64fac
Savannah Bennett <savvy.bennett8@gmail.com>
2021-05-29 15:39:36 -0600

    ReactionSchema implemented

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,34 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema({
+    //custom id value to avoid confusion w/ parent Thought _id
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        validate: [({ length }) => length <= 280, 'Your reaction is too long.']
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+},
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
 const ThoughtSchema = new Schema({
     thoughtText: {
         type: String,
@@ -16,7 +44,7 @@ const ThoughtSchema = new Schema({
         type: String,
         required: true
     },
-    reactions: []
+    reactions: [ReactionSchema]
 },
     {
         toJSON: {
