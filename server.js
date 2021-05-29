d75a6585788fa9f07c2213b5ebcad792b73801c4
Savannah Bennett <savvy.bennett8@gmail.com>
2021-05-29 14:57:12 -0600

    ensured connection to the db

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(routes);
+
+//tells which database we want to connect to - shortcut to BELOW:
+//----------------------connects or creates db if !exist     |
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//log mongo queries being executed
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
