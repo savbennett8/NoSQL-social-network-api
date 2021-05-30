07ac6d9e4aea3a1398bd5348d8551cc80b0ebf07
Savannah Bennett <savvy.bennett8@gmail.com>
2021-05-29 18:58:08 -0600

    implemented thought routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-// const thoughtRoutes = require('./thought-routes');
+const thoughtRoutes = require('./thought-routes');
 
 //prefix all user routes with '/users'
 router.use('/users', userRoutes);
 //prefix all thought routes with '/thoughts'
-// router.use('/thoughts', thoughtRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,19 +2,24 @@ const router = require('express').Router();
 
 const {
     //thought route names go here
-} = require('../../controllers/comment-controller');
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought
+} = require('../../controllers/thought-controller');
 
 //set up GET all and POST at /api/thoughts
 router
     .route('/')
-    .get()
-    .post();
+    .get(getAllThoughts)
+    .post(createThought);
 
 //set up GET single thought, PUT, and DELETE by _id at /api/thoughts/:id
 router
     .route('/:id')
-    .get()
-    .put()
-    .delete();
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
 
 module.exports = router;
